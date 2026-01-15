14ba1f611dcaf520d7243e841a4759ef4a6ad606
Too-Zestyy <violet.steers.dev@gmail.com>
2026-01-15 01:49:08 +0000

    Initial commit

---
+++ SocialAPI/Controllers/NoteController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialAPI.Models;
+
+namespace SocialAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class NoteController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<IEnumerable<Note>> Get()
+    {
+        var note = new Note{Title = "Title example", Content = "This is some content!", CreatedAt = DateTime.Now, Id = 1, UpdatedAt = DateTime.Now};
+
+        return Ok(note);
+    }
+}
---
+++ SocialAPI/Models/Note.cs
@@ -0,0 +1,10 @@
+﻿namespace SocialAPI.Models;
+
+public class Note
+{
+    public int Id { get; set; }
+    public required string Title { get; set; }
+    public string Content { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
