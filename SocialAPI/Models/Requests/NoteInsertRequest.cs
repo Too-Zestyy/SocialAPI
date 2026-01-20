ddc4d50fdd9f0699820c40b81e1b62ea8d2487d7
Too-Zestyy <violet.steers.dev@gmail.com>
2026-01-20 03:01:40 +0000

    chore: move source out of extra directory

--- SocialAPI/Models/Requests/NoteInsertRequest.cs
+++
@@ -1,8 +0,0 @@
-namespace SocialAPI.Models.Requests;
-
-public class NoteInsertRequest
-{
-    public required string Title { get; set; }
-    
-    public string? Content { get; set; }
-}
--- SocialAPI/Models/Requests/UserLogin.cs
+++
@@ -1,7 +0,0 @@
-namespace SocialAPI.Models.Requests;
-
-public class UserLogin
-{
-    public required String Username { get; set; }
-    public required String Password { get; set; }
-}
